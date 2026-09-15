812a77a1b30c935ef6fe0f8fd37bad3063abdc2d
Александр <154451067+HudieGit@users.noreply.github.com>
2026-09-15 16:02:59 +0300

    Update sweep.h

--- include/sweep.h
+++ include/sweep.h
@@ -2,16 +2,21 @@
 #include <string>
 #include <vector>
 
+// удобная структура для передачи в методы sweep, добавил для удобства. один раз создал структуру - везде ее передаешь
+
 struct TridiagonalSystem {
     std::size_t n = 0;
-
+    
+    // kp - это каппа из лекции
     double kp1 = 0.0, mu1 = 0.0;
     double kp2 = 0.0, mu2 = 0.0;
 
     std::vector<double> A, C, B, phi;
-
+    
+    // конструктор 
     explicit TridiagonalSystem(std::size_t nodes = 0) { resize(nodes); }
 
+    // инициализация | переопределение структуры
     void resize(std::size_t nodes) {
         n = nodes;
         A.assign(n + 1, 0.0);
@@ -22,6 +27,7 @@ struct TridiagonalSystem {
 };
 
 
+//класс интерфейс для наследников. I означает, что класс не может иметь своей реализации, а является только типом для своих наследников
 class ISweepStrategy {
 public:
     virtual ~ISweepStrategy() = default;
@@ -31,16 +37,19 @@ protected:
 };
 
 
-
+//конкретная реализация - класс реализующий стандартную прогонку 
 class GenetalSweepStrategy : public ISweepStrategy {
 public:
     std::vector<double> sweep(const TridiagonalSystem& system) const override;
 };
 
+//конкретная реализация - класс реализующий оптимизированную прогонку
 class OptimizedSweepStrategy : public ISweepStrategy {
 public:
     std::vector<double> sweep(const TridiagonalSystem& system) const override {
 
     }
 };
+
+//зачем это все надо? в каком нибудь методе где будет использоваться прогонка в аргументах можно будет написать IsweepStrategy& strategy, но передавать можно в этот метод уже объекты любых двух наследников. и в зависимости от того, что мы передадим метод будет вести себя по разному. в нашем случае если передадим оптимизированну, то, например, скорость работы будет выше, если же мы передавали общий то меньше
 
